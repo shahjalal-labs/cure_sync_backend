9fe89c9088681ba1a7d7b904c4426285e5a8021c
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-19 12:56:11 +0600

    refactor: Updated index.ts, package.json

--- index.ts
+++ index.ts
@@ -1,18 +1,2 @@
-import { PrismaClient } from "./generated/prisma";
-
-const prisma = new PrismaClient();
-
-async function main() {
-  // ... you will write your Prisma Client queries here
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+console.log("Hello via Bun!");
 
