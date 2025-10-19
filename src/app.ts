349b84054ec67d195b969aeb76c372068fdd5736
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-19 19:32:36 +0600

    feat: Added User/user.controller.ts, User/user.routes.ts, User/user.service.ts, User/user.ts, User/user.validation.ts, app/refractorAppPrompt.md | refactor: Updated src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 //
 import express, { Application } from "express";
 import cors from "cors";
+import { UserRoutes } from "./app/module/User/user.js";
 
 export const app: Application = express();
 
 app.use(cors());
 
+app.use("/api/v1/user", UserRoutes);
 app.get("/", (req, res) => {
   res.send({
     message: "cure_sync server running...",
---
+++ src/app/module/User/user.controller.ts
@@ -0,0 +1,11 @@
+//
+
+const create_admin = async (req, res) => {
+  res.send({
+    message: "Hello World",
+  });
+};
+
+export const UserController = {
+  create_admin,
+};
