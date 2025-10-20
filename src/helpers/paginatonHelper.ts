fc98d7468251cda409881a702b152dedd71d705f
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-20 15:30:56 +0600

    refactor: Updated helpers/paginatonHelper.ts

--- src/helpers/paginatonHelper.ts
+++ src/helpers/paginatonHelper.ts
@@ -1,36 +1,3 @@
 //
-type IOptions = {
-  page?: number;
-  limit?: number;
-  sortOrder?: string;
-  sortBy?: string;
-};
 
-type IOptionsResult = {
-  page: number;
-  limit: number;
-  skip: number;
-  sortBy: string;
-  sortOrder: string;
-};
-
-const calculatePagination = (options: IOptions): IOptionsResult => {
-  const page: number = options.page || 1;
-  const limit: number = options.limit || 10;
-  const skip = (page - 1) * limit;
-
-  const sortBy: string = options.sortBy || "createdAt";
-  const sortOrder: string = options.sortOrder || "desc";
-
-  return {
-    page,
-    limit,
-    skip,
-    sortBy,
-    sortOrder,
-  };
-};
-
-export const paginationHelper = {
-  calculatePagination,
-};
+type TOptions = {};
