6003b445b6f49f9ded5ccd44c0c5eba09eb94c35
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-20 09:35:33 +0600

    feat: Added shared/obj.js, shared/pick.ts | refactor: Updated Admin/admin.service.ts | chore: Removed User/refractorUserPrompt.md

--- src/app/module/Admin/admin.service.ts
+++ src/app/module/Admin/admin.service.ts
@@ -1,5 +1,13 @@
 //
-const getAllFromDB = async () => {};
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const getAllFromDB = async () => {
+  const result = await prisma.admin.findMany();
+  return result;
+};
 
 export const AdminService = {
   getAllFromDB,
---
+++ src/shared/obj.js
@@ -0,0 +1,6 @@
+//
+const intro = {};
+
+intro["name"] = "sj";
+
+console.log(intro, "[1;31mintro in obj.js at line 3[0m");
