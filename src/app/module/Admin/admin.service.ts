8f7f4b9d84f44edadc87978354048271e64e0248
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-20 10:17:33 +0600

    refactor: Updated Admin/admin.service.ts

--- src/app/module/Admin/admin.service.ts
+++ src/app/module/Admin/admin.service.ts
@@ -5,23 +5,33 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getAllFromDB = async (search: string) => {
+  const andConditions = [
+    {
+      name: {
+        contains: search,
+        mode: "insensitive",
+      },
+    },
+
+    {
+      email: {
+        contains: search,
+        mode: "insensitive",
+      },
+    },
+  ];
+
+  if (search) {
+    andConditions.push();
+  }
+
+  console.dir(andConditions, {
+    depth: "infinity",
+  });
+
   const result = await prisma.admin.findMany({
     where: {
-      OR: [
-        {
-          name: {
-            contains: search,
-            mode: "insensitive",
-          },
-        },
-
-        {
-          email: {
-            contains: search,
-            mode: "insensitive",
-          },
-        },
-      ],
+      OR: andConditions,
     },
   });
   return result;
