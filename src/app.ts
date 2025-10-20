285096ebcf5fea4cd1354ffa09c962482979bfb1
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-20 08:44:34 +0600

    feat: Added Admin/admin.constant.ts, Admin/admin.controller.ts, Admin/admin.routes.ts, Admin/admin.service.ts | refactor: Updated src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,17 @@
 import express, { Application } from "express";
 import cors from "cors";
 import { UserRoutes } from "./app/module/User/user.routes";
+import { AdminRoutes } from "./app/module/Admin/admin.routes";
 
 export const app: Application = express();
 
 app.use(cors());
 //pareser
+app.use(express.json());
+
 app.use("/api/v1/user", UserRoutes);
+app.use("/api/v1/admin", AdminRoutes);
+
 app.get("/", (req, res) => {
   res.send({
     message: "cure_sync server running...",
