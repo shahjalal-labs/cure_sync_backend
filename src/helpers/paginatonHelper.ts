873b0531e8df7629235ae17077014f600d8f758a
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-20 15:39:48 +0600

    refactor: Updated helpers/paginatonHelper.ts

--- src/helpers/paginatonHelper.ts
+++ src/helpers/paginatonHelper.ts
@@ -1,3 +1,34 @@
 //
 
-type TOptions = {};
+type TOptions = {
+  page?: number;
+  limit?: number;
+  sortOrder?: string;
+  sortBy?: string;
+};
+
+type TOptionsResult = {
+  page: number;
+  limit: number;
+
+  sortOrder: string;
+  sortBy: string;
+  skip: number;
+};
+
+const calcalutePagination = (options: TOptions): TOptionsResult => {
+  const page = options.page || 1;
+  const limit = options.limit || 10;
+  const skip = (page - 1) * limit;
+
+  const sortOrder = options.sortOrder || "desc";
+
+  const sortBy = options.sortBy || "createdAt";
+  return {
+    page,
+    limit,
+    skip,
+    sortOrder,
+    sortBy,
+  };
+};
