1c135f41f196472ecaa780af4d2fc323e2bef72f
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-21 12:12:30 +0600

    refactor: Updated middlewares/validateRequest.ts

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -1,9 +1,16 @@
-import { ZodTypeAny } from "zod";
+//
+import { NextFunction, Request, Response } from "express";
+import { ZodObject, ZodRawShape } from "zod";
 
-export const validateRequest = (schema: ZodTypeAny) => {
-  async (req, res, next) => {
-    console.log(`validate middlware checking`, req.originalUrl);
-
-    throw new Error("Not implemented");
+export const validateRequest =
+  (schema: ZodObject<ZodRawShape>) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({
+        body: req.body,
+      });
+      return next();
+    } catch (error) {
+      next(error);
+    }
   };
-};
