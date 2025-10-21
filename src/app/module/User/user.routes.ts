f261030d2bed955dc0be68d6bdd07dc0fce6722e
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-21 21:58:46 +0600

    refactor: Updated User/user.routes.ts

--- src/app/module/User/user.routes.ts
+++ src/app/module/User/user.routes.ts
@@ -1,13 +1,32 @@
 //
-import express, { Router } from "express";
+import express, {
+  RequestHandler,
+  Router,
+  Request,
+  Response,
+  NextFunction,
+} from "express";
 import { UserController } from "./user.controller";
 import { UserRole } from "@prisma/client";
+import { jwtHelpers } from "../../../helpers/jwtHelpers";
+import config from "../../../config";
+import { Secret } from "jsonwebtoken";
 const router: Router = express.Router();
 
 const auth =
   (...roles: string[]) =>
-  (req, res, next) => {
-    console.log(`roles`, roles);
+  async (req: Request & { user: any }, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization;
+    if (!token) throw new Error("You are not authorized");
+
+    const verifiedUser = jwtHelpers.verifyToken(
+      token,
+      config.jwt.jwt_secret as Secret,
+    );
+    req.user = verifiedUser;
+    if (roles.length && roles.includes(verifiedUser.role)) {
+      next();
+    }
   };
 
 router.post(
