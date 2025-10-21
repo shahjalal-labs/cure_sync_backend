f8db3d8f9f9f52078809dd4b0789c66527c87d05
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-21 11:35:20 +0600

    refactor: Updated User/user.controller.ts, User/user.service.ts

--- src/app/module/User/user.controller.ts
+++ src/app/module/User/user.controller.ts
@@ -3,6 +3,7 @@ import { RequestHandler } from "express";
 import { UserService } from "./user.service";
 import { sendResponse } from "../../../shared/sendResponse";
 import httpStatus from "http-status";
+import catchAsync from "../../../shared/catchAsync";
 
 const createAdmin: RequestHandler = async (req, res) => {
   const result = await UserService.createAdminIntoDB(req.body);
@@ -12,23 +13,15 @@ const createAdmin: RequestHandler = async (req, res) => {
   });
 };
 
-const getAllUsers: RequestHandler = async (req, res) => {
-  try {
-    const result = await UserService.getAllUsersFromDB();
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "All users fetched successfully",
-      data: result,
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: error.message || "Something went wrong",
-      data: error,
-    });
-  }
-};
+const getAllUsers: RequestHandler = catchAsync(async (req, res) => {
+  const result = await UserService.getAllUsersFromDB();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "All users fetched successfully",
+    data: result,
+  });
+});
 
 export const UserController = {
   createAdmin,
--- src/app/module/User/user.service.ts
+++ src/app/module/User/user.service.ts
@@ -4,14 +4,7 @@ import bcrypt from "bcrypt";
 import { prisma } from "../../../shared/prisma";
 
 const createAdminIntoDB = async (data: any) => {
-  console.log(data, "[1;31mdata in user.service.ts at line 8[0m");
-  console.log(`password`, data.password);
   const hashedPassword = await bcrypt.hash(data?.password, 12);
-  console.log(
-    hashedPassword,
-    "[1;31mhashedPassword in user.service.ts at line 7[0m",
-  );
-
   const userData = {
     email: data.admin.email,
     password: hashedPassword,
