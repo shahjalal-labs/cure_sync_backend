1070a458075eaf2d58c497b7d758d348cae47f78
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-21 15:44:47 +0600

    refactor: Updated Auth/auth.service.ts

--- src/app/module/Auth/auth.service.ts
+++ src/app/module/Auth/auth.service.ts
@@ -1,8 +1,27 @@
+import { UserStatus } from "@prisma/client";
+import { prisma } from "../../../shared/prisma";
+import bcrypt from "bcrypt";
+
 type ILoginPayload = {
   email: string;
   password: string;
+};
+
 const loginUserIntoDB = async (payload: ILoginPayload) => {
   console.log(`logging in user`);
+  const userData = await prisma.user.findFirstOrThrow({
+    where: {
+      email: payload.email,
+      status: UserStatus.ACTIVE,
+    },
+  });
+
+  const isPasswordCorrect = await bcrypt.compare(
+    payload.password,
+    userData.password,
+  );
+
+  if (!isPasswordCorrect) throw new Error("Invalid password");
 };
 
 export const AuthService = {
