194fcd44da91abee06816e0615353c922ea251bd
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-21 22:05:46 +0600

    refactor: Updated User/user.routes.ts, User/userApi.hurl

--- src/app/module/User/user.routes.ts
+++ src/app/module/User/user.routes.ts
@@ -15,25 +15,27 @@ const router: Router = express.Router();
 
 const auth =
   (...roles: string[]) =>
-  async (req: Request & { user: any }, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
-    if (!token) throw new Error("You are not authorized");
+  async (req: Request & { user?: any }, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) throw new Error("You are not authorized");
 
-    const verifiedUser = jwtHelpers.verifyToken(
-      token,
-      config.jwt.jwt_secret as Secret,
-    );
-    req.user = verifiedUser;
-    if (roles.length && roles.includes(verifiedUser.role)) {
+      const verifiedUser = jwtHelpers.verifyToken(
+        token,
+        config.jwt.jwt_secret as Secret,
+      );
+      req.user = verifiedUser;
+
+      if (roles.length && !roles.includes(verifiedUser.role)) {
+        throw new Error("Forbidden");
+      }
       next();
+    } catch (error: any) {
+      next(error);
     }
   };
 
-router.post(
-  "/",
-  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
-  UserController.createAdmin,
-);
+router.post("/", auth(UserRole.SUPER_ADMIN), UserController.createAdmin);
 router.get("/", UserController.getAllUsers);
 
 export const UserRoutes: Router = router;
