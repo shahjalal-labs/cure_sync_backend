af6bc87b19ac89c04b4a4dab439dac7dbe33da56
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-23 22:14:07 +0600

    refactor: Updated Admin/admin.routes.ts

--- src/app/module/Admin/admin.routes.ts
+++ src/app/module/Admin/admin.routes.ts
@@ -3,28 +3,10 @@ import express from "express";
 import { AdminController } from "./admin.controller";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { AdminValidationSchema } from "./admin.validation";
-import { jwtHelpers } from "../../../helpers/jwtHelpers";
 
 const router = express.Router();
 
-router.get(
-  "/",
-  (r, s, n) => {
-    const token = jwtHelpers.generateToken(
-      {
-        userid: "userId",
-        role: "ADMIN",
-      },
-      "secret",
-      "1h",
-    );
-
-    console.log(token, "[1;31mtoken in admin.routes.ts at line 14[0m");
-
-    n();
-  },
-  AdminController.getAll,
-);
+router.get("/", AdminController.getAll);
 router.get("/:id", AdminController.getAdminById);
 router.patch(
   "/:id",
