4414b3a3dee70510d26dca8477f60bf2112d01bd
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-23 16:48:42 +0600

    refactor: Updated Schedule/schedule.service.ts

--- src/app/module/Schedule/schedule.service.ts
+++ src/app/module/Schedule/schedule.service.ts
@@ -1,10 +1,29 @@
 //
+
+import { prisma } from "../../../shared/prisma";
+
+//w: (start)╭──────────── createScheduleIntoDB  ────────────╮
+const createScheduleIntoDB = async (payload: any) => {
+  const { startDate, endDate, startTime, endTime } = payload;
+
+  const currentDate = new Date(startDate);
+};
+//w: (end) ╰──────────── createScheduleIntoDB  ────────────╯
+
+//w: (start)╭────────────  ────────────╮
+
+const getScheduleByIdFromDB = async (id: string) => {
+  const result = await prisma.schedule.findUnique({
+    where: {
+      id,
+    },
+  });
+  return result;
+};
+//w: (end) ╰────────────  ────────────╯
 //w: (start)╭────────────  ────────────╮
 
 //w: (end) ╰────────────  ────────────╯
-
-const createScheduleIntoDB = async (payload: any) => {};
-
 export const ScheduleService = {
   createScheduleIntoDB,
 };
