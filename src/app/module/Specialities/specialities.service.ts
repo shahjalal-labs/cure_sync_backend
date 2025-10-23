64a07e735be0d536ed81ae51fa93fece8ce506d3
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-23 14:56:20 +0600

    refactor: Updated Specialities/specialities.service.ts

--- src/app/module/Specialities/specialities.service.ts
+++ src/app/module/Specialities/specialities.service.ts
@@ -3,7 +3,9 @@ import { Request } from "express";
 import { IFile } from "../../interfaces/file";
 import { fileUploader } from "../../../helpers/fileUploader";
 import { prisma } from "../../../shared/prisma";
+import { Specialities } from "@prisma/client";
 
+//w: (start)╭──────────── createSpecialitiesIntoDB  ────────────╮
 const createSpecialitiesIntoDB = async (req: Request) => {
   const file = req.file as IFile;
   if (file) {
@@ -11,9 +13,31 @@ const createSpecialitiesIntoDB = async (req: Request) => {
     req.body.icon = uploadToCloudinary?.secure_url;
   }
 
-  const result = await prisma;
+  const result = await prisma.specialities.create({
+    data: req.body,
+  });
+  return result;
 };
+//w: (end) ╰──────────── createSpecialitiesIntoDB  ────────────╯
 
+//w: (start)╭──────────── getAll specialities from db   ────────────╮
+const getAllSpecialitiesFromDB = async (): Promise<Specialities[]> => {
+  return await prisma.specialities.findMany();
+};
+//w: (end) ╰──────────── getAll specialities from db  ────────────╯
+
+//w: (start)╭──────────── deleteSpecialitiesFromDB  ────────────╮
+const deleteSpecialitiesFromDB = async (id: string): Promise<Specialities> => {
+  const result = await prisma.specialities.delete({
+    where: {
+      id,
+    },
+  });
+  return result;
+};
+//w: (end) ╰──────────── deleteSpecialitiesFromDB  ────────────╯
 export const SpecialitiesService = {
   createSpecialitiesIntoDB,
+  getAllSpecialitiesFromDB,
+  deleteSpecialitiesFromDB,
 };
