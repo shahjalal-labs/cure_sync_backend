470d3bd3124288b458c3413ba3e620498e834fcf
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-26 22:51:02 +0600

    feat: Added Patient/patient.interface.ts | refactor: Updated ck.md, Patient/patient.service.ts, User/user.validation.ts

--- src/app/module/Patient/patient.service.ts
+++ src/app/module/Patient/patient.service.ts
@@ -1,10 +1,16 @@
 //
 
+import { Patient } from "@prisma/client";
+import { paginationHelper } from "../../../helpers/paginatonHelper";
 import { prisma } from "../../../shared/prisma";
+import { IPaginationOptions } from "../../interfaces/pagination";
 
 //w: (start)╭──────────── getAllPatient  ────────────╮
-const getAllPatient = async () => {
-  console.log(`okay`);
+const getAllPatient = async (
+  filters,
+  options: IPaginationOptions,
+): Promise<Patient[] | null> => {
+  const { page, limit, skip } = paginationHelper.calcalutePagination(options);
 
   const result = await prisma.patient.findMany({
     where: {
@@ -18,6 +24,7 @@ const getAllPatient = async () => {
 //w: (start)╭────────────  ────────────╮
 
 //w: (end) ╰────────────  ────────────╯
+
 export const PatientService = {
   getAllPatient,
 };
