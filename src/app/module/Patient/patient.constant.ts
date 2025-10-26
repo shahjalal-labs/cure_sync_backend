81367f7a3aa5027b5a854795f83476667c75c519
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-26 23:05:02 +0600

    feat: Added Patient/patient.constant.ts | refactor: Updated Patient/patient.interface.ts, Patient/patient.service.ts

---
+++ src/app/module/Patient/patient.constant.ts
@@ -0,0 +1,7 @@
+//
+export const patientSearchableFields = [
+  "name",
+  "email",
+  "contactNumber",
+  "searchTerm",
+];
--- src/app/module/Patient/patient.service.ts
+++ src/app/module/Patient/patient.service.ts
@@ -1,17 +1,27 @@
 //
 
-import { Patient } from "@prisma/client";
+import { Patient, Prisma } from "@prisma/client";
 import { paginationHelper } from "../../../helpers/paginatonHelper";
 import { prisma } from "../../../shared/prisma";
 import { IPaginationOptions } from "../../interfaces/pagination";
+import { IPatientFilterRequest } from "./patient.interface";
 
 //w: (start)╭──────────── getAllPatient  ────────────╮
 const getAllPatient = async (
-  filters,
+  filters: IPatientFilterRequest,
   options: IPaginationOptions,
 ): Promise<Patient[] | null> => {
-  const { page, limit, skip } = paginationHelper.calcalutePagination(options);
+  // const { page, limit, skip } = paginationHelper.calcalutePagination(options);
 
+  const { searchTerm, ...filterData } = filters;
+
+  const andConditions: Prisma.PatientWhereInput[] = [];
+
+  if (searchTerm) {
+    andConditions.push({
+        OR: 
+    })
+  }
   const result = await prisma.patient.findMany({
     where: {
       isDeleted: false,
