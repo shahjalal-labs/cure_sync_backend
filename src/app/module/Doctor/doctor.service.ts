a97ac0b6a85d526c36828f1cd096756d6067c49a
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-26 08:28:17 +0600

    refactor: Updated prisma/schema.prisma, Doctor/doctor.service.ts

--- src/app/module/Doctor/doctor.service.ts
+++ src/app/module/Doctor/doctor.service.ts
@@ -1,3 +1,4 @@
+import { Doctor } from "@prisma/client";
 import { prisma } from "../../../shared/prisma";
 
 //w: (start)╭──────────── getAllDoctor  ────────────╮
@@ -7,7 +8,29 @@ const getAllDoctor = async () => {
 };
 //w: (end) ╰──────────── getAllDoctor  ────────────╯
 //
+//w: (start)╭──────────── updateDoctor  ────────────╮
+const updateDoctor = async (
+  id: string,
+  payload: Partial<Doctor>,
+): Promise<Doctor | null> => {
+  const { specialities, ...doctorData } = payload;
+  const existingDoctor = await prisma.doctor.findUniqueOrThrow({
+    where: { id },
+  });
+
+  await prisma.$transaction(async (tx) => {
+    await tx.doctor.update({
+      where: {
+        id,
+      },
+      data: doctorData,
+    });
+  });
+  return null;
+};
+//w: (end) ╰──────────── updateDoctor  ────────────╯
 
 export const DoctorService = {
   getAllDoctor,
+  updateDoctor,
 };
