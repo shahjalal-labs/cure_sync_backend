24d98fea88e172aa18a0df63a20b96bfca084e05
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-28 08:22:43 +0600

    refactor: Updated Patient/patient.validation.ts

--- src/app/module/Patient/patient.validation.ts
+++ src/app/module/Patient/patient.validation.ts
@@ -1,14 +1,33 @@
+//
 import z from "zod";
 
 const updatePatientSchema = z.object({
   body: z
     .object({
-      name: z.string().min(3),
-      contactNumber: z.string().min(4),
-      address: z.string(),
-      patientHealthData: z.object().optional(),
+      name: z.string().min(3).optional(),
+      contactNumber: z.string().min(4).optional(),
+      address: z.string().optional(),
+      patientHealthData: z
+        .object({
+          gender: z.enum(["MALE", "FEMALE"]),
+          dateOfBirth: z.string().min(6),
+          bloodGroup: z.enum([
+            "A_POSITIVE",
+            "B_POSITIVE",
+            "O_POSITIVE",
+            "AB_POSITIVE",
+            "A_NEGATIVE",
+            "B_NEGATIVE",
+            "O_NEGATIVE",
+            "AB_NEGATIVE",
+          ]),
+          hasAllergies: z.boolean().default(false),
+          hasDiabetes: z.boolean().default(false),
+          height: z.string().min(2),
+          weight: z.string().min(2),
+        })
+        .optional(),
     })
-    .partial()
     .strict(),
 });
 
