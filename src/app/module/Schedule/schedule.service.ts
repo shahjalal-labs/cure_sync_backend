5940e468f9b95d456fd6f041fc13bc608c862d00
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-10-31 22:22:29 +0600

    refactor: Updated package.json, Schedule/schedule.service.ts

--- src/app/module/Schedule/schedule.service.ts
+++ src/app/module/Schedule/schedule.service.ts
@@ -1,6 +1,6 @@
 //
-
 import { prisma } from "../../../shared/prisma";
+import { addDays, addMinutes, isBefore, format } from "date-fns";
 
 //w: (start)╭──────────── createScheduleIntoDB  ────────────╮
 const createScheduleIntoDB = async (payload: any) => {
@@ -8,6 +8,39 @@ const createScheduleIntoDB = async (payload: any) => {
 
   const currentDate = new Date(startDate);
 };
+
+async function insertIntoDB({ startDate, endDate, startTime, endTime }) {
+  const slots = [];
+  const interval = 30; // minutes
+
+  for (
+    let day = new Date(startDate);
+    day <= new Date(endDate);
+    day = addDays(day, 1)
+  ) {
+    const dateStr = format(day, "yyyy-MM-dd");
+
+    let start = new Date(`${dateStr}T${startTime}:00`);
+    const end = new Date(`${dateStr}T${endTime}:00`);
+
+    while (isBefore(start, end)) {
+      slots.push({
+        startDateTime: start,
+        endDateTime: addMinutes(start, interval),
+      });
+      start = addMinutes(start, interval);
+    }
+  }
+
+  await prisma.schedule.createMany({
+    data: slots,
+    skipDuplicates: true,
+  });
+
+  console.log("✅ Created slots:", slots.length);
+  return slots;
+}
+
 //w: (end) ╰──────────── createScheduleIntoDB  ────────────╯
 
 //w: (start)╭──────────── getScheduleByIdFromDB  ────────────╮
