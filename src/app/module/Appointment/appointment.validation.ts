2cac5a8e57cd024dcea776f83a8636b998c39a62
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-09 19:31:52 +0600

    refactor: Updated prisma/schema.prisma, Appointment/appointment.validation.ts

--- src/app/module/Appointment/appointment.validation.ts
+++ src/app/module/Appointment/appointment.validation.ts
@@ -1,6 +1,15 @@
 //
 
-const createAppointmenSchema = {};
+import z from "zod";
+
+//w: (start)╭──────────── createAppointmen ────────────╮
+const createAppointmenSchema = {
+  body: z.object({
+    doctorId: z.string().uuid(),
+    scheduleId: z.string().uuid(),
+  }),
+};
+//w: (end)  ╰──────────── createAppointmen ────────────╯
 
 export const AppointmentValidation = {
   createAppointmenSchema,
