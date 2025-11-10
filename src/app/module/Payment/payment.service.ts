cc2e6a491be09016337a92c62128012c7ebd5568
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-10 21:19:07 +0600

    refactor: Updated Payment/payment.service.ts

--- src/app/module/Payment/payment.service.ts
+++ src/app/module/Payment/payment.service.ts
@@ -1,26 +1,40 @@
 //
 
+import axios from "axios";
 import config from "../../../config";
-import SSLCommerzPayment from "sslcommerz-lts";
+import { prisma } from "../../../shared/prisma";
 
 //w: (start)╭──────────── initPayment ────────────╮
 const initPayment = async (appointmentId: string) => {
+  const paymentData = await prisma.payment.findUniqueOrThrow({
+    where: {
+      id: appointmentId,
+    },
+    include: {
+      appointment: {
+        include: {
+          patent: true,
+        },
+      },
+    },
+  });
+
   const data = {
     store_id: config.ssl.storeId,
     store_passwd: config.ssl.storePass,
     // is_live: false,
-    total_amount: 100,
+    total_amount: paymentData.amount,
     currency: "BDT",
-    tran_id: "REF123", // use unique tran_id for each api call
+    tran_id: paymentData.transactionId, // use unique tran_id for each api call
     success_url: "http://localhost:3030/success",
-    fail_url: "http://localhost:3030/fail",
-    cancel_url: "http://localhost:3030/cancel",
+    fail_url: config.ssl.successUrl,
+    cancel_url: config.ssl.cancelUrl,
     ipn_url: "http://localhost:3030/ipn",
-    shipping_method: "Courier",
-    product_name: "Computer.",
-    product_category: "Electronic",
+    shipping_method: "N/A",
+    product_name: "Appointment",
+    product_category: "Healh Care",
     product_profile: "general",
-    cus_name: "Customer Name",
+    cus_name: paymentData.appointment.patent.name,
     cus_email: "customer@example.com",
     cus_add1: "Dhaka",
     cus_add2: "Dhaka",
@@ -38,6 +52,19 @@ const initPayment = async (appointmentId: string) => {
     ship_postcode: 1000,
     ship_country: "Bangladesh",
   };
+
+  const response = await axios({
+    method: "post",
+    url: config.ssl.sslPaymentApi,
+    data,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+  console.log(
+    response.data,
+    "[1;31mresponse in payment.service.ts at line 44[0m",
+  );
 };
 //w: (end)  ╰──────────── initPayment ────────────╯
 
