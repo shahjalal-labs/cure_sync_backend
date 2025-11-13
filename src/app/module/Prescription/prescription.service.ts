f73428ea4498b53f7def91a47db3a4f04e25c97c
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-13 19:12:04 +0600

    refactor: Updated Prescription/prescription.service.ts

--- src/app/module/Prescription/prescription.service.ts
+++ src/app/module/Prescription/prescription.service.ts
@@ -1,6 +1,9 @@
+//
 import { Prescription } from "@prisma/client";
 import { IAuthUser } from "../../interfaces/common";
 import { prisma } from "../../../shared/prisma";
+import { ApiError } from "../../errors/ApiError";
+import httpStatus from "http-status";
 
 const createPrescripton = async (
   user: IAuthUser,
@@ -10,5 +13,23 @@ const createPrescripton = async (
     where: {
       id: payload.appointmentId,
     },
+    include: {
+      doctor: true,
+    },
   });
+
+  if (!(user.email === appointmentData.doctor.email)) {
+    throw new ApiError(httpStatus.BAD_REQUEST, "This is not your appointment!");
+  }
+
+  const result = await prisma.prescription.create({
+    data: {
+      appointmentId: appointmentData.id,
+      doctorId: appointmentData.doctorId,
+    },
+  });
+};
+
+export const PrescriptionService = {
+  createPrescripton,
 };
